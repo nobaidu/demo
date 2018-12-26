613e607e0778844086be0902b1788c5864616728
nobaidu <baidunotsix@gmail.com>
2018-12-26 11:23:24 +0800

    30sjs

--- 30code/30s.js
+++ 30code/30s.js
@@ -1,4 +1,6 @@
-//1.arrayMax
+//1.arrayMax Array.from(arrayLike[, mapFn[, thisArg]])从一个类似数组或可迭代对象中创建一个新的数组实例。
+//.apply() call() bind()
+
 function _toConsumableArray(arr) { 
     if (Array.isArray(arr)) { 
         for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) 
@@ -14,3 +16,8 @@ function _toConsumableArray(arr) {
 var arrayMax = function arrayMax(arr) {
   return Math.max.apply(Math, _toConsumableArray(arr));
 };
+//console.log(Math.max(...[1,87,8]))
+function a(){
+    console.log(arguments.length)
+}
+a(1,'s',2);
