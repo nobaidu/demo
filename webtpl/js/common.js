5a0eaac8096aef2bea6e9e7eb5ee2dbd74a834fd
nobaidu <baidunotsix@gmail.com>
2019-01-19 18:06:17 +0800

    tpl

--- webtpl/js/common.js
+++ webtpl/js/common.js
@@ -1,23 +1,49 @@
-(function () {
-    var deviceWidth;
-    var html = document.getElementsByTagName('html')[0];
+$(function(){
+    $('.m-menu').click(function(){
+        $(this).hide();
+        $('.head').show();
+        $('.m-close').show();
+        $('body').css({'height':'100vh','overflow':'hidden'});
+    })
+    $('.m-close').click(function(){
+        $(this).hide();
+        $('.head').hide();
+        $('.m-menu').show();
+        $('body').css({'height':'auto'});
+    })
+})
 
-    function setHtmlFontSizePc() {
-        deviceWidth = document.documentElement.clientWidth >= 1200 ? 1200 : document.documentElement.clientWidth;
-        html.style.cssText = 'font-size:' + deviceWidth / 12 + 'px !important';
-    }
-    function setHtmlFontSizeMb() {
-        deviceWidth = document.documentElement.clientWidth <= 768 ? document.documentElement.clientWidth : 768;
-        html.style.cssText = 'font-size:' + deviceWidth / 7.68 + 'px !important';
-    }
-    setHtmlFontSizePc();
-    setHtmlFontSizeMb();
+$(function(){
+    $('.pop-btn').click(function(){
+        $('.pop-outer').hide();
+    })
+    $('.pop-outer').click(function(){
+        $(this).hide();
+    })
+})
 
-    window.addEventListener('resize', function () {
-        if (document.documentElement.clientWidth > 768) {
-            setHtmlFontSizePc();
-        } else {
-            setHtmlFontSizeMb();
+//表单验证
+$(function(){
+    $('.btn').click(function(){
+        if($('.name').val()==""){
+            $('.name').focus();
+
+            $('.pop-text').text('姓名不能为空');
+            $('.pop-outer').show();
+            return false;
+        } if($('.tel').val()==""){
+            $('.tel').focus();
+
+            $('.pop-text').text('电话不能为空');
+            $('.pop-outer').show();
+            return false;
+        } if($('.tel').val().length<11 && $('.tel').val().length>0){
+            $('.tel').focus();
+            
+            $('.pop-text').text('电话格式错误');
+            $('.pop-outer').show();
+            return false;
         }
     })
-})()
+});
+
