7a27cc767511064ae8b9a477b2a44598f7bed889
notgit <1174522524@qq.com>
2019-02-18 10:09:30 +0800

    search

---
+++ search/data.js
@@ -0,0 +1,104 @@
+var dataList = {
+  "name": "项目简介",
+  "list": {
+    "canyin": [
+      {
+        "name": "龙潮烤鱼",
+        "tag": "烤鱼",
+        "imgUrl": "cy-lcky.png",
+        "url": "http://lc.guanyi666.com/",
+        "content": "龙的传承，世界的潮流。专利壁炉、果木碳烤、先吃后涮、一鱼两吃。鱼跃龙门，龙潮引领烤鱼美食新风尚。"
+      },
+      {
+        "name": "初客牛排",
+        "tag": "牛排",
+        "imgUrl": "cy-cknp.png",
+        "url": "http://ck.guanyi666.com/",
+        "content": "平价好牛排，就到初客来。初客现煎原切牛排，最初的才是最好的。DIY自助喷枪炙烤，5分钟变身西餐大厨。"
+      },
+      {
+        "name": "鱻煮艺火锅",
+        "tag":"火锅",
+        "imgUrl": "cy-xzy.png",
+        "url": "http://xzy.guanyi666.com/",
+        "content": "一锅纳四海，九州遍奇鲜。好汤底，煲出来，百锅千味，群鲜荟萃。鱻煮艺煲汤火锅，志在打造正宗、美味、营养的火锅美味。"
+      },
+      {
+        "name": "锅得缸坛子焖肉",
+        "tag": "焖肉",
+        "imgUrl": "cy-gdg.ong",
+        "url": "http://gdg.guanyi666.com/",
+        "content": "做不羁的自己，焖尽天下美味。健康成为食尚主流，以锅得缸坛子焖肉为代表的国内轻食类菜品迅速走红！"
+      },
+      {
+        "name": "辣么拽干锅无骨鸭掌",
+        "tag": "火锅",
+        "imgUrl": "cy-lmzyz.png",
+        "url": "http://lmz.guanyi666.com/",
+        "content": "不拽，不青春。辣么拽干锅无骨鸭掌以无骨为特色，以干锅的烹饪手法，先吃后涮，一锅多吃，营养美味兼具，充分迎合了消费者的就餐需求。"
+      }
+    ],
+    "yule": [
+      {
+        "name": "万像国际影视",
+        "imgUrl": "yl-wx.png",
+        "url": "http://wx.guanyi666.com/",
+        "content": "中国私人微影院中的佼佼者，集观影、K歌、上网、直播、VR游戏、餐饮等多种娱乐形式于一身，是一家享有海量片库、独立空间、娱乐一体、24小时营业的多元化娱乐休闲空间。"
+      },
+      {
+        "name": "mini唱吧",
+        "imgUrl": "yl-mncb.png",
+        "url": "http://cb.guanyi666.com/",
+        "content": "万千想象 不如一唱"
+      },
+      {
+        "name": "超凡未来",
+        "imgUrl": "yl-cfwl.png",
+        "url": "http://vr.guanyi666.com/",
+        "content": "超凡未来 为你而来"
+      }
+    ],
+    "meirong": [
+      {
+        "name": "月满馨国际产后恢复中心",
+        "imgUrl": "mr-ymx.png",
+        "url": "http://ymx.guanyi666.com/",
+        "content": "以月嫂服务为中心，涵盖产后修复，婴儿游泳，催乳、育婴、小儿推拿、汗蒸、护工、营养师、月子会所等业务的完整产业链，提供个性化的服务并深得广大客户的认可，是客户心目中富有爱心和责任心的企业。"
+      },
+      {
+        "name": "玫可儿",
+        "imgUrl": "mr-mke.png",
+        "url": "http://micoer.guanyi666.com/",
+        "content": "拥抱轻奢主义，拥有豆蔻年华！"
+      }
+    ],
+    "fuwu": [
+      {
+        "name": "洁巧家政",
+        "imgUrl": "fw-jq.png",
+        "url": "http://jq.guanyi666.com/",
+        "content": "绿色生活 源于洁巧"
+      },
+      {
+        "name": "众视好",
+        "imgUrl": "fw-zsh.png",
+        "url": "http://zsh.yingyungongshang.com/",
+        "content": "众视好"
+      }
+    ],
+    "jiaoyu": [
+      {
+        "name": "超能金脑",
+        "imgUrl": "jy-cnjn.png",
+        "url": "http://jn.guanyi666.com/",
+        "content": "由专业记忆大师组成的研发团队，在理论与实践相结合的基础上，开发出一系列快速记忆课程。通过科学的记忆训练，有效提高学生的记忆力，把方法和学习内容相结合，让学生记得住，记得牢，学习过程轻松快乐，成就优异，让学生开心，家长放心。"
+      },
+      {
+        "name": "华才初航",
+        "imgUrl": "jy-hcch.png",
+        "url": "http://hc.guanyi666.com/",
+        "content": "起航 既世界"
+      }
+    ]
+  }
+}
---
+++ search/dataList.js
@@ -0,0 +1,443 @@
+var dataList = [
+    {
+        "name": "龙潮烤鱼",
+        "tag": "龙潮烤鱼",
+        "imgUrl": "cy-lcky.png",
+        "url": "http://lc.guanyi666.com/",
+        "content": "龙的传承，世界的潮流。专利壁炉、果木碳烤、先吃后涮、一鱼两吃。鱼跃龙门，龙潮引领烤鱼美食新风尚。"
+    },
+    {
+        "name": "初客牛排",
+        "tag": "初客牛排西餐",
+        "imgUrl": "cy-cknp.png",
+        "url": "http://ck.guanyi666.com/",
+        "content": "平价好牛排，就到初客来。初客现煎原切牛排，最初的才是最好的。DIY自助喷枪炙烤，5分钟变身西餐大厨。"
+    },
+    {
+        "name": "鱻煮艺火锅",
+        "tag": "鱻煮艺火锅",
+        "imgUrl": "cy-xzy.png",
+        "url": "http://xzy.guanyi666.com/",
+        "content": "一锅纳四海，九州遍奇鲜。好汤底，煲出来，百锅千味，群鲜荟萃。鱻煮艺煲汤火锅，志在打造正宗、美味、营养的火锅美味。"
+    },
+    {
+        "name": "锅得缸坛子焖肉",
+        "tag": "锅得缸坛子焖肉小吃",
+        "imgUrl": "cy-gdg.ong",
+        "url": "http://gdg.guanyi666.com/",
+        "content": "做不羁的自己，焖尽天下美味。健康成为食尚主流，以锅得缸坛子焖肉为代表的国内轻食类菜品迅速走红！"
+    },
+    {
+        "name": "辣么拽干锅无骨鸭掌",
+        "tag": "辣么拽干锅无骨鸭掌小吃",
+        "imgUrl": "cy-lmzyz.png",
+        "url": "http://lmz.guanyi666.com/",
+        "content": "不拽，不青春。辣么拽干锅无骨鸭掌以无骨为特色，以干锅的烹饪手法，先吃后涮，一锅多吃，营养美味兼具，充分迎合了消费者的就餐需求。"
+    },
+    {
+        "name": "辣么拽铁锅牛蛙",
+        "tag": "辣么拽铁锅牛蛙",
+        "imgUrl": ".png",
+        "url": "http://nw.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "壹锅蒸能量",
+        "tag": "壹锅蒸能量",
+        "imgUrl": ".png",
+        "url": "http://ygz.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "双侠记",
+        "tag": "双侠记火锅",
+        "imgUrl": ".png",
+        "url": "http://sxj.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "半城山色",
+        "tag": "半城山色涮烤一体火锅",
+        "imgUrl": ".png",
+        "url": "http://bcss.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "翅宴",
+        "tag": "翅宴木桶鱼",
+        "imgUrl": ".png",
+        "url": "http://lc.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "福祺道",
+        "tag": "福祺道鱼火锅",
+        "imgUrl": ".png",
+        "url": "http://fqd.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "传香古色",
+        "tag": "传香古色瓦罐小吃",
+        "imgUrl": ".png",
+        "url": "http://gscx.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "华客多",
+        "tag": "华客多牛肉汉堡",
+        "imgUrl": ".png",
+        "url": "http://hkd.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "五福茉莉香",
+        "tag": "五福茉莉香鸭爪鸭脖小吃",
+        "imgUrl": ".png",
+        "url": "http://mlx.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "虾模蟹样",
+        "tag": "虾模蟹样干锅",
+        "imgUrl": ".png",
+        "url": "http://xmxy.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "匠码头",
+        "tag": "匠码头海鲜",
+        "imgUrl": ".png",
+        "url": "http://jmt.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "左岸食尚",
+        "tag": "左岸食尚牛排西餐",
+        "imgUrl": ".png",
+        "url": "http://np.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "乔客来",
+        "tag": "乔客来包子",
+        "imgUrl": ".png",
+        "url": "http://qkl.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "走秀鸡排",
+        "tag": "走秀鸡排",
+        "imgUrl": ".png",
+        "url": "http://zx.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "我的样",
+        "tag": "我的样鸡排",
+        "imgUrl": ".png",
+        "url": "http://wdy.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "煲百味",
+        "tag": "煲百味快餐",
+        "imgUrl": ".png",
+        "url": "http://bbw.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "牛九段",
+        "tag": "牛九段烧烤肉",
+        "imgUrl": ".png",
+        "url": "http://njd.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "嘻哈鸡",
+        "tag": "嘻哈鸡火锅",
+        "imgUrl": ".png",
+        "url": "http://xhj.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "榴莲裙",
+        "tag": "榴莲裙披萨",
+        "imgUrl": ".png",
+        "url": "http://pizza.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "小心蹄你",
+        "tag": "小心蹄你猪蹄烧烤",
+        "imgUrl": ".png",
+        "url": "http://kzt.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "嘿饭你",
+        "tag": "嘿饭你快餐",
+        "imgUrl": ".png",
+        "url": "http://hfn.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "烟火猎人",
+        "tag": "烟火猎人烧烤",
+        "imgUrl": ".png",
+        "url": "http://kr.guanyi666.com/",
+        "content": ""
+    },
+    // {
+    //     "name": "俏食尚",
+    //     "tag": "",
+    //     "imgUrl": ".png",
+    //     "url": "",
+    //     "content": ""
+    // },
+    {
+        "name": "柠檬草的味道",
+        "tag": "柠檬草的味道茶饮",
+        "imgUrl": ".png",
+        "url": "http://nmc.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "水煮三国",
+        "tag": "水煮三国",
+        "imgUrl": ".png",
+        "url": "http://szsg.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "肠粉传说",
+        "tag": "肠粉传说",
+        "imgUrl": ".png",
+        "url": "http://cfcs.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "丸小侠",
+        "tag": "丸小侠章鱼小丸子",
+        "imgUrl": ".png",
+        "url": "http://wxx.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "嗷嗷牛",
+        "tag": "嗷嗷牛牛排西餐",
+        "imgUrl": ".png",
+        "url": "http://aan.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "快乐火车",
+        "tag": "快乐火车旋转小火锅",
+        "imgUrl": ".png",
+        "url": "http://xhg.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "蜜朵果恋",
+        "tag": "蜜朵果恋茶饮",
+        "imgUrl": ".png",
+        "url": "http://mdgl.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "泡饼煮义",
+        "tag": "泡饼煮义",
+        "imgUrl": ".png",
+        "url": "http://pb.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "辣美季",
+        "tag": "辣美季鱼锅",
+        "imgUrl": ".png",
+        "url": "http://lmj.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "烧烤总动员",
+        "tag": "烧烤总动员",
+        "imgUrl": ".png",
+        "url": "http://sk.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "膳宝坊",
+        "tag": "膳宝坊卤煮",
+        "imgUrl": ".png",
+        "url": "http://sbf.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "美滋淋",
+        "tag": "美滋淋奶茶",
+        "imgUrl": ".png",
+        "url": "http://mzl.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "黄椒主",
+        "tag": "黄椒主麻辣香锅",
+        "imgUrl": ".png",
+        "url": "http://hjz.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "摩择茶语",
+        "tag": "摩择茶语茶饮",
+        "imgUrl": ".png",
+        "url": "http://mzcy.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "全鱼道",
+        "tag": "全鱼道",
+        "imgUrl": ".png",
+        "url": "http://qyd.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "饼饼有礼",
+        "tag": "饼饼有礼煎饼",
+        "imgUrl": ".png",
+        "url": "http://bbyl.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "爆肚超人",
+        "tag": "爆肚超人小吃",
+        "imgUrl": ".png",
+        "url": "http://bd.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "煎饼密码",
+        "tag": "煎饼密码",
+        "imgUrl": ".png",
+        "url": "http://jbmm.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "摩珂珂",
+        "tag": "摩珂珂咖啡",
+        "imgUrl": ".png",
+        "url": "http://mkk.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "豆腐侠",
+        "tag": "豆腐侠",
+        "imgUrl": ".png",
+        "url": "http://dfx.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "京卷食代",
+        "tag": "京卷食代卷饼",
+        "imgUrl": ".png",
+        "url": "http://juan.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "手串手",
+        "tag": "手串手串串麻辣烫",
+        "imgUrl": ".png",
+        "url": "http://scs.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "皇琦雪冰",
+        "tag": "皇琦雪冰茶饮",
+        "imgUrl": ".png",
+        "url": "http://xb.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "炒粉骑士",
+        "tag": "炒粉骑士",
+        "imgUrl": ".png",
+        "url": "http://cf.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "瑞余烤鱼堂",
+        "tag": "瑞余烤鱼堂",
+        "imgUrl": ".png",
+        "url": "http://ry.guanyi666.com/",
+        "content": ""
+    },
+    {
+        "name": "万像国际影视",
+        "tag": "万像国际影视娱乐影院",
+        "imgUrl": "yl-wx.png",
+        "url": "http://wx.guanyi666.com/",
+        "content": "中国私人微影院中的佼佼者，集观影、K歌、上网、直播、VR游戏、餐饮等多种娱乐形式于一身，是一家享有海量片库、独立空间、娱乐一体、24小时营业的多元化娱乐休闲空间。"
+    },
+    {
+        "name": "mini唱吧",
+        "tag": "mini唱吧娱乐k歌",
+        "imgUrl": "yl-mncb.png",
+        "url": "http://cb.guanyi666.com/",
+        "content": "万千想象 不如一唱"
+    },
+    {
+        "name": "超凡未来",
+        "tag": "超凡未来娱乐vr",
+        "imgUrl": "yl-cfwl.png",
+        "url": "http://vr.guanyi666.com/",
+        "content": "超凡未来 为你而来"
+    },
+    {
+        "name": "月满馨",
+        "tag": "月满馨国际产后恢复中心美容服务",
+        "imgUrl": "mr-ymx.png",
+        "url": "http://ymx.guanyi666.com/",
+        "content": "以月嫂服务为中心，涵盖产后修复，婴儿游泳，催乳、育婴、小儿推拿、汗蒸、护工、营养师、月子会所等业务的完整产业链，提供个性化的服务并深得广大客户的认可，是客户心目中富有爱心和责任心的企业。"
+    },
+    {
+        "name": "玫可儿",
+        "tag": "玫可儿女士美容",
+        "imgUrl": "mr-mke.png",
+        "url": "http://micoer.guanyi666.com/",
+        "content": "拥抱轻奢主义，拥有豆蔻年华！"
+    },
+    {
+        "name": "爵醒空间",
+        "tag": "爵醒空间男士美容",
+        "imgUrl": "mr-jxkj.png",
+        "url": "http://juexing888.com/",
+        "content": ""
+    },
+    {
+        "name": "洁巧家政",
+        "tag": "洁巧家政服务",
+        "imgUrl": "fw-jq.png",
+        "url": "http://jq.guanyi666.com/",
+        "content": "绿色生活 源于洁巧"
+    },
+    {
+        "name": "众视好",
+        "tag": "众视好视力服务",
+        "imgUrl": "fw-zsh.png",
+        "url": "http://zsh.yingyungongshang.com/",
+        "content": "众视好"
+    },
+    {
+        "name": "超能金脑",
+        "tag": "超能金脑右脑开发幼儿教育早教",
+        "imgUrl": "jy-cnjn.png",
+        "url": "http://jn.guanyi666.com/",
+        "content": "由专业记忆大师组成的研发团队，在理论与实践相结合的基础上，开发出一系列快速记忆课程。通过科学的记忆训练，有效提高学生的记忆力，把方法和学习内容相结合，让学生记得住，记得牢，学习过程轻松快乐，成就优异，让学生开心，家长放心。"
+    },
+    {
+        "name": "华才初航",
+        "tag": "华才初航右脑开发幼儿教育早教",
+        "imgUrl": "jy-hcch.png",
+        "url": "http://hc.guanyi666.com/",
+        "content": "起航 既世界"
+    }
+]
