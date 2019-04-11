aa96681f83fd6b3d1df79255650c072d51fce376
notgit <1174522524@qq.com>
2019-04-11 11:52:40 +0800

    webtpl

--- webtpl/js/common.js
+++ webtpl/js/common.js
@@ -1,49 +1,54 @@
-$(function(){
-    $('.m-menu').click(function(){
+$(function () {
+    $('.m-menu').click(function () {
         $(this).hide();
         $('.head').show();
         $('.m-close').show();
-        $('body').css({'height':'100vh','overflow':'hidden'});
+        $('body').css({
+            'overflow-x': 'hidden'
+        });
     })
-    $('.m-close').click(function(){
+    $('.m-close').click(function () {
         $(this).hide();
         $('.head').hide();
         $('.m-menu').show();
-        $('body').css({'height':'auto'});
+        $('body').css({
+            'overflow-y': 'auto'
+        });
     })
 })
 
-$(function(){
-    $('.pop-btn').click(function(){
+$(function () {
+    $('.pop-btn').click(function () {
         $('.pop-outer').hide();
     })
-    $('.pop-outer').click(function(){
+    $('.pop-outer').click(function () {
         $(this).hide();
     })
 })
 
 //表单验证
-$(function(){
-    $('.btn').click(function(){
-        if($('.name').val()==""){
+$(function () {
+    $('.btn').click(function () {
+        if ($('.name').val() == "") {
             $('.name').focus();
 
             $('.pop-text').text('姓名不能为空');
             $('.pop-outer').show();
             return false;
-        } if($('.tel').val()==""){
+        }
+        if ($('.tel').val() == "") {
             $('.tel').focus();
 
             $('.pop-text').text('电话不能为空');
             $('.pop-outer').show();
             return false;
-        } if($('.tel').val().length<11 && $('.tel').val().length>0){
+        }
+        if ($('.tel').val().length < 11 && $('.tel').val().length > 0) {
             $('.tel').focus();
-            
+
             $('.pop-text').text('电话格式错误');
             $('.pop-outer').show();
             return false;
         }
     })
 });
-
