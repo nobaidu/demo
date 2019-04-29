c76121cc8143e6b515e4da1c9f823b6232ae5494
notgit <1174522524@qq.com>
2019-04-29 17:37:53 +0800

    'h5'

--- javascript/prototype/api.js
+++ javascript/prototype/api.js
@@ -6,23 +6,23 @@
  * 
  * setPrototypeOf
  * isPrototypeOf
-*/   
-function Human(){
+ */
+function Human() {
 
 }
 Human.prototype = {
-    run: function(){
+    run: function () {
         console.log('run')
     }
 }
 
-function Chinese(){
+function Chinese() {
 
 }
 // 操作原型
 Chinese.prototype = Object.setPrototypeOf({
     constructor: Chinese,
-    say: function(name){
+    say: function (name) {
         console.log(name);
     }
 }, Human.prototype);
@@ -40,3 +40,16 @@ Object.defineProperty(ming, 'age', {
     configurable: true,
     writable: true
 });
+
+/*
+    继承方式
+    1.原型链继承
+    Chinese.prototype.say = func;
+    2.原型链继承
+    Chinese.protype = {
+        constructor: Chinese,
+        say: func
+    }
+    3.拷贝继承
+    for in 循环
+*/
